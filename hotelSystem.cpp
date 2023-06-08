fbeea816a87d27e668bcb5cdd7676bcb000d4372
Esteban Zavala <116309257+Zavalaesteban1@users.noreply.github.com>
2023-06-08 11:58:08 -0500

    Add files via upload

---
+++ hotelSystem.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string> 
+#include <iomanip>
+
+
+using namespace std;
+// include h file 
+#include "hotelSystem.h"
+
+
+int main()
+{
+    // declare object 
+    Hotel firstGuest;
+    // call the menu 
+    firstGuest.menu(); 
+
+    return 0; 
+}
